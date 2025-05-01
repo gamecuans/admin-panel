7dd9f9bd62e27bc301488d6e38c0cef73a6ec612
gamecuans <watchcash48@gmail.com>
2025-05-02 01:58:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById("logoutBtn").addEventListener("click", function() {
-  alert("Logout clicked! (Fitur ini hanya tampilan statis)");
-});
+function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'GameCuans' && password === 'Juli172022') {
+    window.location.href = 'dashboard.html';
+  } else {
+    document.getElementById('error').innerText = 'Username atau password salah.';
+  }
+}
